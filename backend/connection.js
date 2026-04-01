39954e953b8c1c49cbaea0e4bb0cc1222e64bcb5
shilpisharma12 <limitlessdesign.in@gmail.com>
2026-04-01 09:35:19 +0530

    https://github.com/digipodium/owasp.git

--- backend/connection.js
+++ backend/connection.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+// console.log(process.env.MONGO_URI);
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/owasp-scanner');
+        const conn = await mongoose.connect('mongodb://localhost:27017/owasp-scanner');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
